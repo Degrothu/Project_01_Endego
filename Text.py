b184cb0c7ba05364eca2f2130d48b3d20a999805
David Novotný <ahodak@seznam.cz>
2023-05-29 11:01:03 +0200

    Version_01

--- Text.py
+++ Text.py
@@ -4,7 +4,7 @@ author: David Novotný
 email: ahodak@seznam.cz
 discord: David N.#7946
 """
-import task_template
+from task_template import TEXTS
 
 users = {
     "bob": "123",
@@ -14,67 +14,71 @@ users = {
 }
 
 # přihlášení uživatele
-username = input("Zadejte uživatelské jméno: ")
-password = input("Zadejte heslo: ")
+username = input("Enter a username: ")
+password = input("Enter password: ")
 
-if users.get(username) == password:
-    print("-" * 30, f"Welcome to the app, {username}", "We have 3 texts to be analyzed.", "-" * 30, sep="\n")
+if users.get(username) != password:
+    print("Uregistered user, terminating the program....")
+    exit()
 
-    # výběr textu k analýze
-    selected_text = input("Vyberte text k analýze (1, 2, nebo 3): ")
-    print("-" * 30)
+print("-" * 30, f"Welcome to the app, {username}", "We have 3 texts to be analyzed.", "-" * 30, sep="\n")
 
-    if selected_text.isdigit() and int(selected_text) in range(1, 4):
-        words = task_template.TEXTS[int(selected_text) - 1].split()
-        word_count = len(words)
-        title_count = 0
-        upper_count = 0
-        lower_count = 0
-        digit_count = 0
-        sum_numbers = 0
+# výběr textu k analýze
+selected_text = input("Select the text to analyze (1, 2, 3): ")
+print("-" * 30)
 
-        for word in words:
-            if word.istitle():
-                title_count += 1
-            elif word.isupper():
-                upper_count += 1
-            elif word.islower():
-                lower_count += 1
-            elif word.isdigit():
-                digit_count += 1
-                sum_numbers += int(word)
+if not selected_text.isdigit() or int(selected_text) not in range(1, 4):
+    print("Invalid text selection, terminating the program..")
+    exit()
 
-        print(f"Počet slov: {word_count}")
-        print(f"Počet slov začínajících velkým písmenem: {title_count}")
-        print(f"Počet slov psaných velkými písmeny: {upper_count}")
-        print(f"There are {lower_count} lowercase words.")
-        print(f"There are {digit_count} numeric strings.")
-        print(f"The sum of all the numbers are {sum_numbers}")
+words = TEXTS[int(selected_text) - 1].split(" ")
+word_count = len(words)
+title_count = 0
+upper_count = 0
+lower_count = 0
+digit_count = 0
+sum_numbers = 0
 
-        word_lengths = {}
+for word in words:
+    if word.istitle():
+        title_count += 1
+    elif word.isupper():
+        upper_count += 1
+    elif word.islower():
+        lower_count += 1
+    elif word.isdigit():
+        digit_count += 1
+        sum_numbers += int(word)
 
-        # projdeme všechna slova a spočítáme jejich délky
-        for word in words:
-            length = len(word)
-            if length in word_lengths:
-                word_lengths[length] += 1
-            else:
-                word_lengths[length] = 1
+print(f"There are {word_count} words in the selected text.")
+print(f"There are {title_count} titlecase words.")
+print(f"There are {upper_count} uppercase words.")
+print(f"There are {lower_count} lowercase words.")
+print(f"There are {digit_count} numeric strings.")
+print(f"The sum of all the numbers are {sum_numbers}")
 
-        # inicializujeme řetězec pro výstup
-        output = ""
+word_lengths = {}
 
-        # projdeme všechny délky slov a vytvoříme řetězec s délkou slov a hvězdičkami
-        for length in sorted(word_lengths.keys()):
-            output += f'{length}| {"*" * word_lengths[length]} {word_lengths[length]}\n'
-
-        # vytiskneme řetězec s délkou slov a hvězdičkami
-        print("-" * 30)
-        print(f"LEN |OCCURENCES |NR.")
-        print("-" * 30, output, sep="\n")
-
+# projdeme slova a spočítáme jejich délky
+for word in words:
+    length = len(word)
+    if length in word_lengths:
+        word_lengths[length] += 1
     else:
-        print("incorrect values, terminating the program..")
+        word_lengths[length] = 1
+
+# projdeme všechny délky slov a vytvoříme řetězec s délkou slov a hvězdičkami
+output = ""
+for length in sorted(word_lengths.keys()):
+    output += f'{length:2}|{"*" * word_lengths[length]:16}|{word_lengths[length]:2}\n'
+
+# vytiskneme hlavičku a oddělovací čáru
+print("-" * 30)
+print("EN|  OCCURENCES    |NR.")
+print("-" * 30)
+
+# vytiskneme řetězec s délkou slov a hvězdičkami
+print(output)
 
 
 
@@ -82,5 +86,6 @@ if users.get(username) == password:
 
 
 
-else:
-    print("unregistered user, terminating the program..")
+
+
+
